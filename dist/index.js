e0ccede153c91ae7f33352661ab6afe3bebc4568
KwakuAldo <x.pac20032001@gmail.com>
2023-05-12 11:14:53 +0000

    added option to show password in plain text

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+const passwordInput = document.getElementById('password');
+const showPasswordCheckbox = document.getElementById('showPassword');
+
+showPasswordCheckbox.addEventListener('change', function () {
+    if (showPasswordCheckbox.checked) {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+    console.log("CheckBox")
+});
