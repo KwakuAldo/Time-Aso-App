caa2d41e7b7ad319e518d0b778d173389e380a8f
KwakuAldo <x.pac20032001@gmail.com>
2023-05-13 16:49:56 +0000

    added sign up page

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 const passwordInput = document.getElementById('password');
 const showPasswordCheckbox = document.getElementById('showPassword');
+const phoneInput = document.getElementById('telephone');
 
 showPasswordCheckbox.addEventListener('change', function () {
     if (showPasswordCheckbox.checked) {
@@ -7,4 +8,18 @@ showPasswordCheckbox.addEventListener('change', function () {
     } else {
         passwordInput.type = 'password';
     }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let selectElement = document.querySelectorAll('select');
+    selectElement.forEach(function (selectEl) {
+        selectEl.addEventListener('change', function () {
+            this.removeAttribute('disabled');
+        });
+    });
+});
+
+    // Allow only numeric digits and specific characters
+phoneInput.addEventListener('input', function (event) {
+    this.value = this.value.replace(/[^0-9()+-]/g, '');
 });
